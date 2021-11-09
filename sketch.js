8ac35b05c202451c0fa48e1fc0dcec833f6c0f0d
Girop <65816493+Girop@users.noreply.github.com>
2021-11-09 16:15:24 +0100

    COLOURS

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let populationVal,speedLimit,viewDistance,separationForce,aligmentForce,cohesionForce;
+let populationVal,speedLimit,viewDistance,separationForce,aligmentForce,cohesionForce,p;
 let entities = [];
-let clr = 0;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   // createCanvas(1000, 800);
+
   window.addEventListener('resize', resizeCanvasCallback, false);
   function resizeCanvasCallback() {
     resizeCanvas(windowWidth, windowHeight);
@@ -18,9 +18,9 @@ function setup() {
       factdiv = 1920;
     }
   }
-  populationVal = 110;
+  populationVal = 90;
   speedLimit = 3;
-  viewDistance = 110;
+  viewDistance = 50;
   
   aligmentForce = 0.3;
   separationForce = 0.15;
@@ -28,37 +28,42 @@ function setup() {
 
   colorMode(HSB);
   
-
-  
   //Boids init  
   for(let i = 0;i < populationVal;i++){
-    entities[i] = new Boid(i,random(0,width),random(0,height));
+    entities[i] = new Boid(i,random(0,width),random(0,height),random(0,360));
   }
   
 }
 
 function draw() {
   background(0);
-   clr += 1;
-   clr %= 360;
   for(let i = 0;i < entities.length;i++){
     entities[i].Live(i);
   }
 
 }
 
+// function mouseClicked(){
+//   p = random(0,360);
+//   console.log(p);
+// }
+
 function mouseDragged(){
-  entities.push(new Boid(entities.length + 1,mouseX,mouseY ) );
+  // console.log(p);
+  entities.push(new Boid(entities.length + 1,mouseX,mouseY,100 ) );
 }
 
 
 class Boid{
-  constructor(i,x,y){
+  constructor(i,x,y,passClr){
     this.a = 6;
     this.position = createVector(x,y);
     this.velocity = p5.Vector.random2D();
     this.acc = createVector();
     
+    this.origin_clr = passClr;
+    this.clr = this.origin_clr;
+
     this.index = i;
   }
 
@@ -74,14 +79,19 @@ class Boid{
   NearbyCheck(){
 
     this.nearby = []; 
+    this.VeryNearby = []; 
 
     for(let j = 0;j < entities.length;j++){
       let d = dist(this.position.x,this.position.y,entities[j].position.x,entities[j].position.y);
       if(this.index != j && d < viewDistance ){
         this.nearby.push(entities[j]);
+        
+        if(d < (viewDistance*1.4)){
+          this.VeryNearby.push(entities[j]);
+        }
       }
     }
-
+  
   }
 
   BorderCheck(){
@@ -97,12 +107,40 @@ class Boid{
     }
   }
 
-  Show(){
-    fill(clr,100,100);
-    // stroke(20,180,20,100);
-    // stroke(this.fillclr);
-    // strokeWeight(3);
+  ColourChange(){
+    // let whitness = 0;
+    this.clr = this.origin_clr;
+    
+    
+    if(this.VeryNearby.length > 0){
+      this.VeryNearby.forEach( veryNearBoid =>{
+        this.clr += veryNearBoid.clr;
+      })
+      // whitness = 100;
+      
+      this.clr /= this.VeryNearby.length;
+      
+      // if(this.clr > this.pastClr){
+      //   this.clr += 2;
+      // }else if(this.clr > this.pastClr){
+      //   this.clr -= 2
+      // }
+      
+    } else if(this.clr <  this.origin_clr){
+      this.clr += 1;
+    }else if(this.clr > this.origin_clr){
+      this.clr += 1;
+    }
+    
 
+    this.clr %= 360;
+    fill(this.clr,80,100);
+
+    this.pastClr = this.clr;
+    // console.log(this.pastClr);
+  }
+  
+  Show(){
     push();
     translate(this.position.x,this.position.y);
     rotate(this.velocity.heading());
@@ -124,7 +162,6 @@ class Boid{
 
     this.acc.add(alVec);
   }
-
 
   Separtion(){
     let sepVec = createVector(0,0);
@@ -171,6 +208,7 @@ class Boid{
     
     this.BorderCheck();
     this.Move();
+    this.ColourChange();
     this.Show();
     
     this.acc.mult(0);
